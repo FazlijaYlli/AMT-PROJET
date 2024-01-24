b80f5d2daa8cf8ed89517c022df2b1c440ed7200
Kylian <42773865+kylianmanzini@users.noreply.github.com>
2024-01-24 20:21:00 +0100

    Update ServersResource.java

--- src/main/java/ch/heigvd/resource/ServersResource.java
+++ src/main/java/ch/heigvd/resource/ServersResource.java
@@ -10,10 +10,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.json.JsonObject;
 import jakarta.persistence.EntityManager;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.SecurityContext;
@@ -44,4 +41,48 @@ public class ServersResource {
 
         return API.createServerListResponse(successMessage, servers);
     }
+
+    @POST
+    @Path("join")
+    @Authenticated
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject joinServer(@Context SecurityContext securityContext,
+                                 @FormParam("serverId") String serverId) {
+
+        // verify if parameters are not null
+        if(serverId == null || serverId.isEmpty()){
+            return API.createErrorResponse("Missing parameters");
+        }
+
+        // verify if server id is a number
+        try{
+            Long.parseLong(serverId);
+        }catch(NumberFormatException e){
+            return API.createErrorResponse("Server id is not a number");
+        }
+
+        // verify if server exists
+        Server server = entityManager.getReference(Server.class, Long.parseLong(serverId));
+        if(server == null){
+            return API.createErrorResponse("Server does not exist");
+        }
+
+        Account user = us.resolve(securityContext);
+
+        // add user to server
+        var userlist = server.getMembers();
+        userlist.add(user);
+        server.setMembers(userlist);
+
+        // add server to user
+        var serverlist = user.getServers();
+        serverlist.add(server);
+        user.setServers(serverlist);
+
+        String successMessage = "Server created successfully";
+
+        return API.createResponse(successMessage, server);
+    }
+
 }
