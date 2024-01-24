25cb4c61bf971420106bc562625c0a6ab27cfc02
PrazTobie <tobie.praz@heig-vd.ch>
2024-01-24 18:14:03 +0100

    Moved /register, /logout and /me to root

--- src/main/java/ch/heigvd/resource/AuthResource.java
+++ src/main/java/ch/heigvd/resource/AuthResource.java
@@ -1,7 +1,13 @@
 package ch.heigvd.resource;
 
+import ch.heigvd.entities.Account;
+import ch.heigvd.utilities.ApiResponse;
 import io.quarkus.security.Authenticated;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.json.JsonObject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -9,14 +15,18 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import java.time.Instant;
 import java.util.Date;
 
-@Path("logout")
+@Path("")
 @ApplicationScoped
 public class AuthResource {
+
+    @Inject
+    EntityManager entityManager;
 
     @ConfigProperty(name = "quarkus.http.auth.form.cookie-name")
     String cookieName;
 
     @POST
+    @Path("logout")
     @Authenticated
     public Response logout() {
         final NewCookie removeCookie = new NewCookie.Builder(cookieName)
@@ -25,5 +35,39 @@ public class AuthResource {
                 .path("/")
                 .build();
         return Response.noContent().cookie(removeCookie).build();
+    }
+
+    //TODO: remove this
+    @GET
+    @Path("me")
+    @Authenticated()
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject me(@Context SecurityContext securityContext) {
+        return ApiResponse.buildResponse(ApiResponse.success().add("user", ((Account)securityContext.getUserPrincipal()).toJsonObjectBuilder()));
+    }
+
+    //TODO: Implement register
+    @POST
+    @Path("register")
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject register(@FormParam("username") String username, @FormParam("email") String email, @FormParam("password") String password) {
+
+        // TODO: Check if username/email is already taken
+
+        // TODO: Check if username/email/password are ok
+
+        // TODO: Implement password hashing / hashing strategy
+
+        Account account = new Account();
+        account.setUsername(username);
+        account.setEmail(email);
+        account.setPassword(password);
+        entityManager.persist(account);
+
+        if (account.getId() == null) {
+            return ApiResponse.buildResponse(ApiResponse.error("User not created"));
+        }
+        return ApiResponse.buildResponse(ApiResponse.success().add("user", account.toJsonObjectBuilder()));
     }
 }
